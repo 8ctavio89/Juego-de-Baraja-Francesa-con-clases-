1f780c83d450823ee75a516a7ebb66d5885ffc96
Zelvax42 <50151743+Zelvax42@users.noreply.github.com>
2020-03-24 14:22:19 -0700

    Agregada función genera_lista_cartas

--- pares.py
+++ pares.py
@@ -6,18 +6,43 @@
 #   Félix López Juan Pablo
 #   López Velásquez Octavio
 #   Serna Navarro Ángel Emilio
-
+''' Juego de Baraja Francesa con Clases '''
 import argparse
 import tarjetas
 import itertools
 import random
 
 
-def genera_lista_cargas():
-   
+def genera_lista_cartas():
+    ''' genera una lista de todas las cartas de la baraja (52 cartas)
+        y regresa una lista de estas
+    '''
+    lista_cartas = list()
+    i = 0
+    for i in range(0,3):
+        if i == 0:
+            figura = "C"
+        elif i == 1:
+            figura = "P"
+        elif i == 2:
+            figura = "T"
+        elif i == 3:
+            figura = "D"
+        for j in range(2,14):
+            if j == 11:
+                valor = "J"
+            elif j == 12:
+                valor = "Q"
+            elif j == 13:
+                valor = "K"
+            elif j == 14:
+                valor = "A"
+            carta_nueva = Carta(figura, valor)
+            lista_cartas.append(carta_nueva)
 
 def main(lista_jugadores, mano):
     if len(lista_jugadores) == 2:  # Tienen que ser solo dos jugadores
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
