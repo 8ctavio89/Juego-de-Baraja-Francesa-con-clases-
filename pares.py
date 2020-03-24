65691b2d725dced9c70e3b4b76ecab30d3d25e73
Zelvax42 <50151743+Zelvax42@users.noreply.github.com>
2020-03-24 14:31:08 -0700

    Corregido genera_lista_cartas

--- pares.py
+++ pares.py
@@ -18,7 +18,6 @@ def genera_lista_cartas():
         y regresa una lista de estas
     '''
     lista_cartas = list()
-    i = 0
     for i in range(0,3):
         if i == 0:
             figura = "C"
@@ -41,7 +40,7 @@ def genera_lista_cartas():
                 valor = str(j)
             carta_nueva = Carta(figura, valor)
             lista_cartas.append(carta_nueva)
-        return lista_cartas
+    return lista_cartas
 
 def main(lista_jugadores, mano):
     if len(lista_jugadores) == 2:  # Tienen que ser solo dos jugadores
