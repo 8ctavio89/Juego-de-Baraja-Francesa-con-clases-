1d1efdd7cea4ae7902f752899ed9f773c115bd5a
Zelvax42 <50151743+Zelvax42@users.noreply.github.com>
2020-03-24 15:58:24 -0700

    Crea jugadores

--- pares.py
+++ pares.py
@@ -12,31 +12,42 @@ import tarjetas
 
 
 def genera_lista_cartas():
-    ''' genera una lista de todas las cartas de la baraja (52 cartas)
+    ''' 
+        genera una lista de todas las cartas de la baraja (52 cartas)
         y regresa una lista de estas
     '''
     lista_cartas = list()
-    for i in range(0,4):
+    for i in range(0, 4):
         if i == 0:
-            figura = "C"
+            figura = "C" # Corazones
         elif i == 1:
-            figura = "P"
+            figura = "P" # Picas
         elif i == 2:
-            figura = "T"
+            figura = "T" # Trébol
         elif i == 3:
-            figura = "D"
-        for valor in range(2,15):
+            figura = "D" # Diamante
+        for valor in range(2, 15): # los valores de las cartas empiezan en 2
             if valor == 14:
-                valor = 20
+                valor = 20 # As vale 20
             carta_nueva = tarjetas.Carta(valor, figura)
             lista_cartas.append(carta_nueva)
+
     return lista_cartas
 
+
 def main(jugadores, mano):
-    if len(jugadores) == 2:  # Tienen que ser solo dos jugadores
+    if (len(jugadores) == 2 and mano <= 26):  # Tienen que ser solo dos jugadores y la mano menor a 52
+        nombre_jugador1 = jugadores[0]
+        nombre_jugador2 = jugadores[1]
         lista_cartas = genera_lista_cartas()
+        print("Son", len(lista_cartas), "cartas")
         for i in lista_cartas:
             print(i)
+        baraja = tarjetas.Baraja(lista_cartas)
+        print(baraja)
+        jugador1 = tarjetas.Jugador(nombre_jugador1, baraja)
+        jugador2 = tarjetas.Jugador(nombre_jugador2, baraja)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
