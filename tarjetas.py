79d5c5ac1c8f9b54ece1d2c7bbb7328376f3b3fe
jpfl15 <juanpfelix@hotmail.com>
2020-03-24 16:13:53 -0700

    Se añadieron parametros Juan Pablo Félix El método Jugador.despliega_mano necesita como parametro la baraja para pasarle el diccionario de la misma baraja al método Carta.__str__

--- tarjetas.py
+++ tarjetas.py
@@ -9,11 +9,11 @@ class Jugador:
         self.puntuacion = 0
         baraja.guarda_jugador(self)
 
-    def despliega_mano(self):
+    def despliega_mano(self,baraja):
         print(self.nombre)
         print("----------")
         for carta in self.mano:
-            print(f"{carta.__str__()}\n")
+            print(f"{carta.__str__(baraja.diccionario_cartas)}")
         print(f"Puntuacion: {self.puntuacion}")
 
 
@@ -25,8 +25,9 @@ class Carta:
         self.valor = valor
         self.figura = figura
 
-    def __str__(self):
-        return f"{self.valor}-{self.figura}"
+    def __str__(self,dict_cartas):
+        carta_cara = dict_cartas[self.valor]
+        return f"{carta_cara}-{self.figura}"
 
 
 class Baraja:
@@ -38,25 +39,25 @@ class Baraja:
     def __init__(self, lista_cartas):
         # El método Genera_lista_cartas() debe de hacer la lista de 52 cartas
         self.diccionario_cartas = {
-            "2": 2,
-            "3": 3,
-            "4": 4,
-            "5": 5,
-            "6": 6,
-            "7": 7,
-            "8": 8,
-            "9": 9,
-            "10": 10,
-            "J": 11,
-            "Q": 12,
-            "K": 13,
-            "A": 20,
+            2: "2",
+            3: "3",
+            4: "4",
+            5: "5",
+            6: "6",
+            7: "7",
+            8: "8",
+            9: "9",
+            10: "10",
+            11: "J",
+            12: "Q",
+            13: "K",
+            20: "A",
         }
         self.figuras = ["C", "P", "T", "D"] # Corazones, Picas, Trébol, Diamante
         self.lista_cartas = lista_cartas
         self.lista_jugadores = []
 
-    # def genera_mano (self):
+    # def genera_mano (self,num_cartas):
     # debe asignar una mano aleatoriamente a cada uno de los jugadores de la lista
 
     def guarda_jugador(self, jugador):
